1266d89364913ba63a5e1c8c59f62dcca92176a2
Knut Buene <knutbuene@dhcp-10-22-74-231.wlan.ntnu.no>
2021-03-18 14:35:51 +0100

    implemented usage of API

--- mobile/core/src/com/sigurvar/distanceduel/Main.java
+++ mobile/core/src/com/sigurvar/distanceduel/Main.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sigurvar.distanceduel.utility.APIController;
+import com.sigurvar.distanceduel.utility.NetworkComponent;
 import com.sigurvar.distanceduel.utility.ServerController;
 import com.sigurvar.distanceduel.utility.StateController;
 
@@ -13,13 +15,18 @@ public class Main extends ApplicationAdapter {
 	private ServerController serverController;
 	SpriteBatch batch;
 	Texture img;
-	
+	APIController APIController;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		serverController = new ServerController();
 		serverController.get();
+		APIController = new APIController();
+		APIController.get(); // API for locations
+
 	}
 
 	@Override
--- mobile/core/src/com/sigurvar/distanceduel/utility/APIController.java
+++ mobile/core/src/com/sigurvar/distanceduel/utility/APIController.java
@@ -1,4 +1,20 @@
 package com.sigurvar.distanceduel.utility;
 
-public class APIController {
+import java.io.IOException;
+
+public class APIController extends NetworkComponent {
+
+    public  APIController(){
+        super();
+    }
+
+    public void get() {
+        try {
+            super.SendRequest("http://api.positionstack.com/v1/forward?access_key=bbf5216ec5ff418aea03e7544e54729d&query=lomveien%207%203124%20t%C3%B8nsberg", "");
+        }catch (IOException e){
+            System.out.println(e);
+            status=1;
+        }
+    }
+
 }
